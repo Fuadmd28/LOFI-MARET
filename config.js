fd45f58ed2615275fe30fc962dc6e7c787fa1119
Fuadmd28 <110433657+Fuadmd28@users.noreply.github.com>
2025-03-04 19:20:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-global.owner = ['6282147781510','6283121003293']
+global.owner = ['6282147781510']
 global.mods = ['6282147781510'] 
 global.prems = ['.']
 global.nameowner = 'YTTA'
 global.numberowner = '6282147781510'
-global.nomorown = '6283121003293'
+global.nomorown = '6282147781510'
 global.sgc = 'nekopoi.care'
 global.namebot = '© 𝙇𝙊𝙁𝙄 𝙄𝙈𝙐𝙏𝙏 v5.0.3 (Public Bot)'
 global.mail = 'support@tioprm.eu.org' 
