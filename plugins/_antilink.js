877c4db1be4e3b73b05f31fbcc4f3a6a135ca024
Fuadmd28 <110433657+Fuadmd28@users.noreply.github.com>
2025-03-16 19:58:52 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,34 +1,23 @@
-const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i;
-const WaLinkRegex = /wa.me\/([0-9])/i;
+let handler = m => m
 
-module.exports.before = async function (m, { isAdmin, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe) return;
-    const chat = global.db.data.chats[m.chat];
-    const isGroupLink = linkRegex.exec(m.text);
-    const isLinkWa = WaLinkRegex.exec(m.text);
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
+  if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
+  let chat = global.db.data.chats[m.chat]
+  let isGroupLink = linkRegex.exec(m.text)
 
-    if (chat.antiLink && m.isGroup) {
-        if (isGroupLink && !isAdmin) {
-            if (isBotAdmin) {
-                const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
-                if (m.text.includes(linkThisGroup)) return true;
-            }
-            if (chat.teks) {
-                m.reply(`_*‼️ Link Group Terdeteksi!*_ Pesan kamu akan dihapus! ❌ ${isBotAdmin ? '' : '\n\n_❬Bot Bukan Admin❭_'}`);
-            }
-            if (isBotAdmin) {
-                await conn.sendMessage(m.chat, { delete: m.key });
-            }
-        }
-    } else if (chat.antiLinkWa && m.isGroup) {
-        if (isLinkWa && !isAdmin) {
-            if (chat.teks) {
-                m.reply(`_*‼️ Link Whatsapp Terdeteksi!*_ Pesan kamu akan dihapus! ❌ ${isBotAdmin ? '' : '\n\n_❬Bot Bukan Admin❭_'}`);
-            }
-            if (isBotAdmin) {
-                await conn.sendMessage(m.chat, { delete: m.key });
-            }
-        }
-    }
-    return;
-};
+  if (chat.antiLink && isGroupLink) {
+    await m.reply(`*「 ANTI LINK 」*\n\nDetected *${await conn.getName(m.sender)}* you have sent the group link!\n\nSorry you will be kicked from this group byee!`)
+    if (isAdmin) return m.reply('*Eh sorry you admins, you will not be kicked. hehe..*')
+    if (!isBotAdmin) return m.reply('*Bots are not admins, how can they kick people _-*')
+    let linkGC = ('https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat))
+    let isLinkconnGc = new RegExp(linkGC, 'i')
+    let isgclink = isLinkconnGc.test(m.text)
+    if (isgclink) return m.reply('*「 ANTI LINK 」*\n\nOrder denied, bot will not kick you.\nBecause the group link itself')
+    await conn.sendMessage(m.chat, { delete: m.key })
+    await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+  }
+  return true
+}
+
+module.exports = handler
