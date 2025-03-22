f8a4c04bc5c6f2e1afdd1383ae61f084d9082ec3
Fuadmd28 <110433657+Fuadmd28@users.noreply.github.com>
2025-03-22 12:57:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,15 @@ global.sgc = 'nekopoi.care'
 global.namebot = '© 𝙇𝙊𝙁𝙄 𝙄𝙈𝙐𝙏𝙏 v5.0.3 (Public Bot)'
 global.mail = 'support@tioprm.eu.org' 
 global.gc = 'https://chat.whatsapp.com/JZr68dt7KlEKiJuvEmWxoX'
-global.fotomu = 'https://pomf2.lain.la/f/cjvxzxjo.jpg'
+global.fotomu = 'https://i.supa.codes/uzCXwc'
 global.menu = 'https://btch.pages.dev/file/ea9a7ed7ad210811c90ff.jpg' //image menu , but not work 
 global.instagram = 'https://instagram.com/erlanrahmat_14'
-global.wm = '© 𝙇𝙊𝙁𝙄 𝙄𝙈𝙐𝙏𝙏 💕'
+global.wm = '© DREAM TEA'
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
 global.packname = 'Made With'
-global.author = '𝙇𝙊𝙁𝙄 𝙄𝙈𝙐𝙏𝙏'
+global.author = 'DREAM TEA'
 global.maxwarn = '2' // Peringatan maksimum
 
 //INI WAJIB DI ISI!//
