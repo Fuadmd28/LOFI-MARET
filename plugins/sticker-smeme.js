b8797ae48c1616d0a951772a2a41f6e34e8d761f
Fuadmd28 <110433657+Fuadmd28@users.noreply.github.com>
2025-03-29 11:43:58 +0700

    Update sticker-smeme.js

--- plugins/sticker-smeme.js
+++ plugins/sticker-smeme.js
@@ -1,38 +1,33 @@
-const FormData = require('form-data');
-const { fromBuffer } = require('file-type');
-const fetch = require('node-fetch')
+const uploadImage = require('../lib/uploadImage.js');
+const { Sticker } = require('wa-sticker-formatter');
 
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+    let [atas, bawah] = text.split('|');
+    let q = m.quoted ? m.quoted : m;
+    let mime = (q.msg || q).mimetype || '';
+    if (!mime) throw `Balas gambar dengan perintah\n\n${usedPrefix + command} <${atas ? atas : 'teks atas'}>|<${bawah ? bawah : 'teks bawah'}>`;
+    if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`;
+    m.reply('Tunggu sebentar...');
+    let img = await q.download();
+    let url = await uploadImage(img);
+    let meme = `https://api.memegen.link/images/custom/${encodeURIComponent(atas ? atas : ' ')}${encodeURIComponent(bawah ? '/' + bawah : '')}.png?background=${url}`;
+    let stiker = await createSticker(meme, false, '', '');
+    await conn.sendFile(m.chat, stiker, '', '', m);
+};
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+handler.help = ['smeme <text>|<text>'];
+handler.tags = ['sticker'];
+handler.command = /^(smeme)$/i;
+handler.limit = 3;
 
-    let [atas, bawah] = text.split`|`
-    let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || ''
-    if (!mime) throw `balas gambar dengan perintah\n\n${usedPrefix + command} <${atas ? atas : 'teks atas'}>|<${bawah ? bawah : 'teks bawah'}>`
-    if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`
-    let img = await q.download()
-    let url = await uploadImage(img)
-    let meme = `https://api.memegen.link/images/custom/${encodeURIComponent(atas ? atas : '')}/${encodeURIComponent(bawah ? bawah : '')}.png?background=${url}`
-    conn.sendImageAsSticker(m.chat, meme, m, { packname: packname, author: author })
+module.exports = handler;
 
-}
-handler.help = ['stickermeme <teks>|<teks>']
-handler.tags = ['sticker']
-handler.command = /^(s(tic?ker)?me(me)?)$/i
-
-handler.limit = false
-
-module.exports = handler
-
-async function uploadImage(buffer) { 
-  let { ext } = await fromBuffer(buffer);
-  let bodyForm = new FormData();
-  bodyForm.append("file", buffer, "file." + ext);
-  let res = await fetch("https://file.btch.rf.gd/api/upload.php", {
-    method: "post",
-    body: bodyForm,
-  });
-  let data = await res.json();
-  let resultUrl = data.result ? data.result.url : '';
-  return resultUrl;
+async function createSticker(img, url, packName, authorName, quality) {
+    let stickerMetadata = {
+        type: 'full',
+        pack: packName,
+        author: authorName,
+        quality: quality || 100
+    };
+    return (new Sticker(img ? img : url, stickerMetadata)).toBuffer();
 }
