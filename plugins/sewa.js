a2c4a19512c866d0cc799b98727aad40e7e77136
Fuadmd28 <110433657+Fuadmd28@users.noreply.github.com>
2025-04-08 21:11:55 +0700

    Update sewa.js

--- plugins/sewa.js
+++ plugins/sewa.js
@@ -1,13 +1,14 @@
 let handler = async (m, { conn, usedPrefix }) => {
 let text = `*_🟢 Open Sewa Bot WhatsApp_*
 
-- *Rp 10,000* _1 minggu_
-- *Rp 30,000* _1 bulan_
-- *Rp 50,000* _permanen_
+- *Rp 5,000* _18 Hari_
+- *Rp 10,000* _36 Hari_
+- *Rp 20,000* _54 Hari_
+- *Rp 35,000* _Permanen_
 
  *Jika minat/mau tanya ² 👤*
- https://wa.me/c/6282338631264`
-conn.sendFile(m.chat, 'https://files.catbox.moe/48no35.jpg', '', text, m)
+ https://wa.me/6282147781510`
+conn.sendFile(m.chat, 'https://files.catbox.moe/p452f2.jpg', '', text, m)
 }
 handler.help = ['sewa']
 handler.tags = ['info','main']
